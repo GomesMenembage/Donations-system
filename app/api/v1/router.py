fc1803409ca85100df099328e27370876c9e480f
GomesMenembage <mikeykkj15@gmail.com>
2026-06-14 09:05:48 +0100

    refator:router.py

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from app.api.v1 import admin, auth, centers, donors
 
-router = APIRouter(prefix="/api/v1")
+router = APIRouter(prefix="/api")
 
 router.include_router(auth.router, prefix="/auth", tags=["Auth"])
 router.include_router(donors.router, prefix="/donors", tags=["Donors"])
